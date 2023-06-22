2782d07c734fd5c639485bf0a7e749f63bd18447
fernando diaz <fernando.diaz.ef@gmail.com>
2023-06-22 12:45:10 -0600

    ajuste contactos1

--- src/containers/contactos/index.js
+++ src/containers/contactos/index.js
@@ -1,20 +1,28 @@
 import { Header } from "../../components/header";
 import './style.css';
-import Email from './imagenes-contactos/email.jpg';
-import Telefono from './imagenes-contactos/contactos_llamar.png';
 import { Footer } from "../../components/footer/";
 import { Form } from 'react-bootstrap'
+import { useState } from "react";
 
 const Contactos = ({ background }) => {
 
-const Contactos = () => {
-    const backGround = false;
+    const [validated, setValidated] = useState(false);
+
+    const submit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        setValidated(true);
+    }
 
     return (
         <>
             <Header />
       
-            <main className={`${backGround ? 'bg-dark' : 'bg-light'}`}>
+            <main className={`${background ? 'bg-dark' : 'bg-light'}`}>
                 <div className="text-center">
                     <div className=" main_contactanos mb-3 p-5 shadow">
                         <div className="d-flex flex-column align-items-center " style={{ width: "100%" }}>
@@ -25,32 +33,32 @@ const Contactos = () => {
                     <div className="d-flex justify-content-sm-around align-items-center text-dark flex-column flex-sm-row ">
 
                         <div className="div_container_contactanos w-75 p-4 rounded">
-                            <Form className={`${backGround ? 'text-white' : 'text-dark'}`}>
+                            <Form noValidate validated={validated} className={`${background ? 'text-white' : 'text-dark'}`} onClick={(event) => submit(event)}>
                                 <p className="text-start">Nombre:</p>
-                                <Form.Control type="text" className={`mb-4 w-100 bg-secondary bg-opacity-25 ${backGround ? 'text-white' : 'text-dark'}`} required placeholder="Ingrese su Nombre" />
+                                <Form.Control type="text" className={`mb-4 w-100 bg-secondary bg-opacity-25 ${background ? 'text-white' : 'text-dark'}`} required placeholder="Ingrese su Nombre" />
                                 <p className="text-start">Correo electrónico:</p>
-                                <Form.Control type="email" className={`mb-4 w-100 bg-secondary bg-opacity-25 ${backGround ? 'text-white' : 'text-dark'}`} required placeholder="Ingrese su Correo" />
+                                <Form.Control type="email" className={`mb-4 w-100 bg-secondary bg-opacity-25 ${background ? 'text-white' : 'text-dark'}`} required placeholder="Ingrese su Correo" />
                                 <p className="text-start">Ingrese su observación:</p>
-                                <Form.Control as='textarea' className={`mb-4 w-100 bg-secondary bg-opacity-25 textarea-style ${backGround ? 'text-white' : 'text-dark'}`} required placeholder="Ingrese su observacion aqui" />
+                                <Form.Control as='textarea' className={`mb-4 w-100 bg-secondary bg-opacity-25 textarea-style ${background ? 'text-white' : 'text-dark'}`} required placeholder="Ingrese su observacion aqui" />
                                 <button type="submit" className="btn btn-info mb-2  p-1 ajuste-boton-enviar" >Enviar</button>
                             </Form>
                         </div>
 
                         <div className="w-50 ">
                             <div className="mb-3">
-                                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="m-3"><i className={`bi bi-facebook i fs-4 ${backGround ? 'text-white-color' : 'text-color'}`}></i></a>
-                                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="m-3"><i className={`bi bi-instagram i fs-4 ${backGround ? 'text-white-color' : 'text-color'}`}></i></a>
-                                <a href="https://twitter.com/?lang=es" target="_blank" rel="noopener noreferrer" className="m-3"><i className={`bi bi-twitter i fs-4 ${backGround ? 'text-white-color' : 'text-color'}`}></i></a>
-                                <a href="https://github.com/ErickPu146/Mango-Park/tree/main" target="_blank" rel="noopener noreferrer" className="m-3"><i className={`bi bi-github i fs-4 ${backGround ? 'text-white-color' : 'text-color'}`}></i></a>
+                                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="m-3"><i className={`bi bi-facebook i fs-4 ${background ? 'text-white-color' : 'text-color'}`}></i></a>
+                                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className="m-3"><i className={`bi bi-instagram i fs-4 ${background ? 'text-white-color' : 'text-color'}`}></i></a>
+                                <a href="https://twitter.com/?lang=es" target="_blank" rel="noopener noreferrer" className="m-3"><i className={`bi bi-twitter i fs-4 ${background ? 'text-white-color' : 'text-color'}`}></i></a>
+                                <a href="https://github.com/ErickPu146/Mango-Park/tree/main" target="_blank" rel="noopener noreferrer" className="m-3"><i className={`bi bi-github i fs-4 ${background ? 'text-white-color' : 'text-color'}`}></i></a>
                             </div>
-                            <div className={`align-items-center mb-5 ${backGround ? 'text-white' : 'text-dark'}`}>
-                                <i className={`bi bi-envelope-paper-fill size ${backGround ? 'text-white-color' : 'text-color'}`}></i>
+                            <div className={`align-items-center mb-5 ${background ? 'text-white' : 'text-dark'}`}>
+                                <i className={`bi bi-envelope-paper-fill size ${background ? 'text-white-color' : 'text-color'}`}></i>
                                 <p>Envíanos un correo:<br />
                                     MangoPark@hotmail.com
                                 </p>
                             </div>
-                            <div className={`mb-5 ${backGround ? 'text-white' : 'text-dark'}`}>
-                                <i className={`bi bi-telephone-fill size ${backGround ? 'text-white-color' : 'text-color'}`}></i>
+                            <div className={`mb-5 ${background ? 'text-white' : 'text-dark'}`}>
+                                <i className={`bi bi-telephone-fill size ${background ? 'text-white-color' : 'text-color'}`}></i>
                                 <p>Contactanos tambien por numero telefonico:<br />
                                     (+502)2514 4155
                                 </p>
